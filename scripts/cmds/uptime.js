70d1ec639dfc9360b83c6d0065dbf931a3fdad44
Hacker226758 <127070765+Hacker226758@users.noreply.github.com>
2024-07-08 08:27:50 +0800

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -23,6 +23,6 @@ module.exports = {
 		const hours = Math.floor((uptime / (60 * 60)) % 24);
 		const days = Math.floor(uptime / (60 * 60 * 24));
 		const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
-		api.sendMessage(`Hello Users💛, the Gilgamesh bot has been running for ${uptimeString}.`, event.threadID);
+		api.sendMessage(`Hello Users💛, the Cid kagenou bot has been running for ${uptimeString}.`, event.threadID);
 	}
 };
